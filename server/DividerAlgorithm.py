97534ce69ddd2289b356c293c19c9557e032be1d
netid <asepulcri@student.tudelft.nl>
2023-01-29 20:14:14 +0100

    made code less painful to look at

--- server/DividerAlgorithm.py
+++ server/DividerAlgorithm.py
@@ -28,104 +28,11 @@ class DividerAlgorithm:
 
     number_of_shapes_per_size: Dict[str, int]
     counter: int
-    shape_1: ShapeDecisionVariable
-    shape_2: ShapeDecisionVariable
-    shape_3: ShapeDecisionVariable
-    shape_4: ShapeDecisionVariable
-    shape_5: ShapeDecisionVariable
-    shape_6: ShapeDecisionVariable
-    shape_7: ShapeDecisionVariable
-    shape_8: ShapeDecisionVariable
-    shape_9: ShapeDecisionVariable
-    shape_10: ShapeDecisionVariable
-    shape_11: ShapeDecisionVariable
-    shape_12: ShapeDecisionVariable
-    shape_13: ShapeDecisionVariable
-    shape_14: ShapeDecisionVariable
-    shape_15: ShapeDecisionVariable
-    shape_16: ShapeDecisionVariable
-    shape_17: ShapeDecisionVariable
-    shape_18: ShapeDecisionVariable
-    shape_19: ShapeDecisionVariable
-    shape_20: ShapeDecisionVariable
-    shape_21: ShapeDecisionVariable
-    shape_22: ShapeDecisionVariable
-    shape_23: ShapeDecisionVariable
-    shape_24: ShapeDecisionVariable
-    shape_25: ShapeDecisionVariable
-    shape_26: ShapeDecisionVariable
-    shape_27: ShapeDecisionVariable
-    shape_28: ShapeDecisionVariable
-    shape_29: ShapeDecisionVariable
-    shape_30: ShapeDecisionVariable
 
     def __init__(self):
         self.board = np.array(np.zeros((BOARD_HEIGHT, BOARD_WIDTH)))
-        self.shapes = []
 
-        self.shape_1 = ShapeDecisionVariable(deepcopy(all_shapes))
-        self.shape_2 = ShapeDecisionVariable(deepcopy(all_shapes))
-        self.shape_3 = ShapeDecisionVariable(deepcopy(all_shapes))
-        self.shape_4 = ShapeDecisionVariable(deepcopy(all_shapes))
-        self.shape_5 = ShapeDecisionVariable(deepcopy(all_shapes))
-        self.shape_6 = ShapeDecisionVariable(deepcopy(all_shapes))
-        self.shape_7 = ShapeDecisionVariable(deepcopy(all_shapes))
-        self.shape_8 = ShapeDecisionVariable(deepcopy(all_shapes))
-        self.shape_9 = ShapeDecisionVariable(deepcopy(all_shapes))
-        self.shape_10 = ShapeDecisionVariable(deepcopy(all_shapes))
-        self.shape_11 = ShapeDecisionVariable(deepcopy(all_shapes))
-        self.shape_12 = ShapeDecisionVariable(deepcopy(all_shapes))
-        self.shape_13 = ShapeDecisionVariable(deepcopy(all_shapes))
-        self.shape_14 = ShapeDecisionVariable(deepcopy(all_shapes))
-        self.shape_15 = ShapeDecisionVariable(deepcopy(all_shapes))
-        self.shape_16 = ShapeDecisionVariable(deepcopy(all_shapes))
-        self.shape_17 = ShapeDecisionVariable(deepcopy(all_shapes))
-        self.shape_18 = ShapeDecisionVariable(deepcopy(all_shapes))
-        self.shape_19 = ShapeDecisionVariable(deepcopy(all_shapes))
-        self.shape_20 = ShapeDecisionVariable(deepcopy(all_shapes))
-        self.shape_21 = ShapeDecisionVariable(deepcopy(all_shapes))
-        self.shape_22 = ShapeDecisionVariable(deepcopy(all_shapes))
-        self.shape_23 = ShapeDecisionVariable(deepcopy(all_shapes))
-        self.shape_24 = ShapeDecisionVariable(deepcopy(all_shapes))
-        self.shape_25 = ShapeDecisionVariable(deepcopy(all_shapes))
-        self.shape_26 = ShapeDecisionVariable(deepcopy(all_shapes))
-        self.shape_27 = ShapeDecisionVariable(deepcopy(all_shapes))
-        self.shape_28 = ShapeDecisionVariable(deepcopy(all_shapes))
-        self.shape_29 = ShapeDecisionVariable(deepcopy(all_shapes))
-        self.shape_30 = ShapeDecisionVariable(deepcopy(all_shapes))
-
-        self.decision_variables = [
-            self.shape_1,
-            self.shape_2,
-            self.shape_3,
-            self.shape_4,
-            self.shape_5,
-            self.shape_6,
-            self.shape_7,
-            self.shape_8,
-            self.shape_9,
-            self.shape_10,
-            self.shape_11,
-            self.shape_12,
-            self.shape_13,
-            self.shape_14,
-            self.shape_15,
-            self.shape_16,
-            self.shape_17,
-            self.shape_18,
-            self.shape_19,
-            self.shape_20,
-            self.shape_21,
-            self.shape_22,
-            self.shape_23,
-            self.shape_24,
-            self.shape_25,
-            self.shape_26,
-            self.shape_27,
-            self.shape_28,
-            self.shape_29,
-            self.shape_30,
-        ]
+        self.decision_variables = [ShapeDecisionVariable(deepcopy(all_shapes)) for i in range(30)]
 
         self.number_of_shapes_per_size = {"1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0}
         self.counter = 1
